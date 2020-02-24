aa971019b35deb949dd677f582d8cf07870f1199
arl5705 <arl5705@rit.edu>
2020-02-24 16:00:17 -0500

    set up basic matrix struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+pub mod matrix {
+    pub struct Matrix {
+        entries: [[i32; usize]; usize]
+    }
+
+    impl Matrix {
+        pub fn new(rows: usize, cols: usize) -> Matrix {
+            Matrix { entries: [[0; rows]; cols] }
+        }
+
+        pub fn identity(size: usize) -> Matrix {
+            let mut entries = [[0; size]; size];
+            for i in 0..size {
+                entries[i][i] = 1;
+            }
+            Matrix { entries }
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
