d47f44fd1da709520b984b14d43b636a3cae88a3
arl5705 <arl5705@rit.edu>
2020-02-24 18:23:15 -0500

    refactored the data structure inside Matrix from a 2D array (evidently a bad idea in rust) to a 1D vector

--- src/lib.rs
+++ src/lib.rs
@@ -19,68 +19,80 @@ pub mod matrix {
         }
     }
 
+    fn cap(a: usize, b: usize) -> Vec<i32> {
+        Vec::with_capacity(a * b - 1)
+    }
+
     pub struct Matrix {
         rows: usize,
         cols: usize,
-        entries: [[i32; usize]; usize]
+        entries: Vec<i32>
     }
 
     impl Matrix {
-        pub fn new(rows: usize, cols: usize) -> Matrix {
+
+        pub fn from(rows: usize, cols: usize, entries: Vec<i32>) -> Matrix {
             Matrix {
                 rows,
                 cols,
-                entries: [[0; rows]; cols]
+                entries
             }
+        }
+
+        pub fn new(rows: usize, cols: usize) -> Matrix {
+            Matrix::from(rows, cols, cap(rows, cols))
         }
 
         pub fn identity(size: usize) -> Matrix {
-            let mut entries = [[0; size]; size];
+            let mut entries = cap(size, size);
             for i in 0..size {
-                entries[i][i] = 1;
+                entries[i * size - 1] = 1;
             }
-            Matrix {
-                rows: size,
-                cols: size,
-                entries
-            }
+            Matrix::from(size, size, entries)
         }
 
-        pub fn transpose(&self) -> Matrix {
-            let mut entries = [[0; self.cols]; self.rows];
-            for (row, col) in (self.rows, self.cols) {
-                entries[col][row] = self.entries[row][col];
-            }
-            Matrix {
-                rows: self.cols,
-                cols: self.rows,
-                entries
-            }
+//        pub fn transpose(&self) -> Matrix {
+//            let num_entries = self.rows * self.cols - 1;
+//            let mut entries = Vec::with_capacity(num_entries);
+//            for i in 1..num_entries {
+//                entries[i] = self.entries[i + self.rows % num_entries]
+//            }
+//            Matrix::from(cols, rows, entries)
+//        }
+
+        pub fn get(&self, row: usize, col: usize) -> i32 {
+            self.entries[((row - 1) * self.cols) + col - 1]
         }
 
-        pub fn row(&self, i: usize) -> Result<[i32; usize], OutOfBoundsError> {
-            if i > 0 && i <= self.rows {
-                Ok(self.entries[i])
-            } else {
-                Err(OutOfBoundsError)
-            }
-        }
+//        pub fn row(&self, i: usize) -> Result<[i32; usize], OutOfBoundsError> {
+//            if i > 0 && i <= self.rows {
+//                Ok(self.entries[i-1])
+//            } else {
+//                Err(OutOfBoundsError)
+//            }
+//        }
 
-        pub fn col(&self, i: usize) -> Result<[i32; usize], OutOfBoundsError> {
-            if i > 0 && i <= self.cols {
-                Ok(self.transpose().row(i)?) //unnecessarily expensive?
-            } else {
-                Err(OutOfBoundsError)
-            }
-        }
+//        pub fn col(&self, i: usize) -> Result<[i32; usize], OutOfBoundsError> {
+//            if i > 0 && i <= self.cols {
+//                Ok(self.transpose().row(i)?) //unnecessarily expensive?
+//            } else {
+//                Err(OutOfBoundsError)
+//            }
+//        }
     }
 }
 
 
 #[cfg(test)]
 mod tests {
+    use crate::matrix::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn from_array() {
+        let mini_mat = [1,2,3,4];
+        let my_matrix = Matrix::from(2, 2, mini_mat);
+        assert_eq!(my_matrix.get(1,1), 1);
+        assert_eq!(my_matrix.get(1,2), 2);
+        assert_eq!(my_matrix.get(2,1), 3);
+        assert_eq!(my_matrix.get(2,2), 4);
     }
 }
