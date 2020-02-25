54300cc3c470be667ab7f47b744fa0355c6b4243
arl5705 <arl5705@rit.edu>
2020-02-25 11:52:38 -0500

    got rid of Entry type, more trouble than it was worth. also got rid of row() and col() as they're superfluous, at least for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,7 @@
 pub mod matrix {
-    use std::ops::Deref;
 
     fn v(r: usize, c: usize) -> Vec<i32> {
         vec![0; r*c]
-    }
-
-    pub struct Entry {
-        row: usize,
-        col: usize,
-        data: i32
-    }
-
-    impl Entry {
-        pub fn new(row: usize, col: usize, data: i32) -> Entry {
-            Entry {
-                row,
-                col,
-                data
-            }
-        }
-
-        pub fn update(&mut self, data: i32) {
-            self.data = data;
-        }
     }
 
     pub enum Operation {
@@ -56,7 +35,7 @@ pub mod matrix {
     pub struct Matrix {
         r: usize,
         c: usize,
-        entries: Vec<Entry>
+        entries: Vec<i32> //todo: put in box?
     }
 
     impl Matrix {
@@ -65,17 +44,7 @@ pub mod matrix {
             Matrix {
                 r,
                 c,
-                entries: entries.iter()
-                    .enumerate()
-                    .map(
-                        |(i,e)|
-                            Entry::new(
-                                (i%c)+1,
-                                (i%r)+1,
-                                e.clone()
-                            )
-                    )
-                    .collect()
+                entries
             }
         }
 
@@ -91,55 +60,31 @@ pub mod matrix {
             Matrix::from(s, s, entries)
         }
 
-        pub fn entry(&self, r: usize, c: usize) -> &Entry {
-            &self.entries[((r - 1) * self.c) + c - 1] //faster and easier than iter bullshit
+        pub fn entry(&self, r: usize, c: usize) -> i32 {
+            self.entries[((r - 1) * self.c) + c - 1] //faster and easier than iter bullshit
         }
 
-        pub fn row(&self, r: usize) -> Vec<(usize, &Entry)> { //todo: validate that r < num rows
-            self.entries
-                .iter()
-                .enumerate()
-                .filter(|(_,e)| e.row == r)
-                .collect()
-        }
-
-        pub fn col(&self, c: usize) -> Vec<(usize, &Entry)> { //todo: validate that c < num cols
-            self.entries
-                .iter()
-                .enumerate()
-                .filter(|(_,e)| e.col == c)
-                .collect()
+        pub fn update(&mut self, r: usize, c: usize, data: i32) {
+            self.entries[((r-1) * self.c) + c - 1] = data
         }
 
         pub fn list(&self) -> Vec<i32> {
-            self.entries
-                .iter()
-                .map(|e| e.data)
-                .collect()
-        }
-
-        fn swap(&mut self, c1: (usize, usize), c2: (usize, usize)) {
-            let mut entry1 = *self.entry(c1.0, c1.1);
-            let mut entry2 = *self.entry(c2.0, c2.1);
-            let temp = entry1.data;
-            entry1.update(entry2.data);
-            entry2.update(temp);
+            self.entries.clone()
         }
 
         pub fn transpose(&self) -> Matrix {
             unimplemented!()
         }
 
-        pub fn row_op(self, op: RowOp) -> Matrix {
-            let mut copy = Matrix::from(self.r, self.c, self.list());
+        pub fn row_op(&mut self, op: RowOp) -> &Self {
             match op.operation {
                 Operation::Swap => {
-                    for col in 0..copy.c {
-                        let entry1 = (op.row1, col);
-                        let entry2 = (op.row2, col);
-                        copy.swap(entry1, entry2);
+                    for col in 0..self.c {
+                        let temp = self.entry(op.row1, col);
+                        self.update(op.row1, col, self.entry(op.row2, col));
+                        self.update(op.row2, col, temp);
                     }
-                    copy
+                    self
                 },
                 Operation::Sum => unimplemented!(),
                 Operation::Multiply => unimplemented!()
@@ -183,26 +128,8 @@ mod tests {
     }
 
     #[test]
-    fn get_row() {
-        let vec = vec![2,4,6,8];
-        let mat = Matrix::from(2,2, vec);
-        let r1 = mat.row(1);
-        assert_eq!(r1.iter().map(|t| t.1).collect::<Vec<i32>>(), vec![2,4]);
-        let r2 = mat.row(2);
-        assert_eq!(r2, vec![(2,6),(3,8)])
-    }
-
-    #[test]
-    fn get_col() {
-        let vec = vec![1,3, 5,7];
-        let mat = Matrix::from(2,2, vec);
-        let c1 = mat.col(1);
-        assert_eq!(c1, vec![(0,1), (2,5)])
-    }
-
-    #[test]
     fn swap_rows() {
-        let matrix = Matrix::from(2,2, vec![1,2,3,4]);
+        let mut matrix = Matrix::from(2,2, vec![1,2,3,4]);
         let original_r1 = matrix.row(1);
         let swap = RowOp::new(Operation::Swap, 1, 2, 0);
         let matrix = matrix.row_op(swap);
