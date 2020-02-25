4abf451409886fc40a0734d75d6864f7a7b91f24
arl5705 <arl5705@rit.edu>
2020-02-24 19:57:15 -0500

    stubbed out various functions: row, col, rref, transpose, as well as a RowOperation struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,65 +1,59 @@
 pub mod matrix {
+    use syntax::util::map_in_place::MapInPlace;
 
-    fn cap(a: usize, b: usize) -> Vec<i32> {
-        Vec::with_capacity(a * b - 1)
+    fn v(r: usize, c: usize) -> Vec<i32> {
+        vec![0; r*c - 1]
     }
 
     pub struct Matrix {
-        rows: usize,
-        cols: usize,
+        r: usize,
+        c: usize,
         entries: Vec<i32>
     }
 
+    struct RowOperation {} //todo
+
     impl Matrix {
 
-        pub fn from(rows: usize, cols: usize, entries: Vec<i32>) -> Matrix {
+        pub fn from(r: usize, c: usize, entries: Vec<i32>) -> Matrix {
             Matrix {
-                rows,
-                cols,
+                r,
+                c,
                 entries
             }
         }
 
-        pub fn new(rows: usize, cols: usize) -> Matrix {
-            Matrix::from(rows, cols, cap(rows, cols))
+        pub fn new(r: usize, c: usize) -> Matrix {
+            Matrix::from(r, c, v(r, c))
         }
 
         pub fn identity(size: usize) -> Matrix {
-            let mut entries = cap(size, size);
+            let mut entries = v(size, size);
             for i in 0..=size {
-                entries[i * size - 1] = 1;
+                entries[i*size - 1] = 1;
             }
             Matrix::from(size, size, entries)
         }
 
-//        pub fn transpose(&self) -> Matrix {
-//            let num_entries = self.rows * self.cols - 1;
-//            let mut entries = Vec::with_capacity(num_entries);
-//            for i in 1..num_entries {
-//                entries[i] = self.entries[i + self.rows % num_entries]
-//            }
-//            Matrix::from(cols, rows, entries)
-//        }
-
-        pub fn entry(&self, row: usize, col: usize) -> i32 {
-            self.entries[((row - 1) * self.cols) + col - 1]
+        pub fn transpose(&self) -> Matrix {
+            unimplemented!()
         }
 
-//        pub fn row(&self, i: usize) -> Result<[i32; usize], OutOfBoundsError> {
-//            if i > 0 && i <= self.rows {
-//                Ok(self.entries[i-1])
-//            } else {
-//                Err(OutOfBoundsError)
-//            }
-//        }
+        pub fn entry(&self, r: usize, c: usize) -> i32 {
+            self.entries[((r - 1) * self.c) + c - 1]
+        }
 
-//        pub fn col(&self, i: usize) -> Result<[i32; usize], OutOfBoundsError> {
-//            if i > 0 && i <= self.cols {
-//                Ok(self.transpose().row(i)?) //unnecessarily expensive?
-//            } else {
-//                Err(OutOfBoundsError)
-//            }
-//        }
+        pub fn row(&self, i: usize) -> Vec<i32> {
+            unimplemented!()
+        }
+
+        pub fn col(&self, i: usize) -> Vec<i32> {
+            unimplemented!()
+        }
+
+        pub fn rref(&self) -> (Matrix, Vec<RowOperation>) {
+            unimplemented!()
+        }
     }
 }
 
@@ -69,11 +63,11 @@ mod tests {
     use crate::matrix::*;
     #[test]
     fn from_array() {
-        let mini_mat = vec![1,2,3,4];
-        let my_matrix = Matrix::from(2, 2, mini_mat);
-        assert_eq!(my_matrix.entry(1,1), 1);
-        assert_eq!(my_matrix.entry(1,2), 2);
-        assert_eq!(my_matrix.entry(2,1), 3);
-        assert_eq!(my_matrix.entry(2,2), 4);
+        let vec = vec![1, 2, 3, 4];
+        let mat = Matrix::from(2, 2, vec);
+        assert_eq!(mat.entry(1, 1), 1);
+        assert_eq!(mat.entry(1, 2), 2);
+        assert_eq!(mat.entry(2, 1), 3);
+        assert_eq!(mat.entry(2, 2), 4);
     }
 }
